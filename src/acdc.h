b825c9e5589d2ff75d11766c8d52d137e6a41c0b
Martin Aigner <martin@maigner.net>
2012-12-27 15:33:05 +0100

    added some statistics for memory objects

--- src/acdc.h
+++ src/acdc.h
@@ -14,11 +14,21 @@ struct mutator_options {
   int thread_id;
 };
 
+//mutator measurement data
+typedef struct mutator_stat MStat;
+struct mutator_stat {
+  unsigned long bytes_allocated;
+  unsigned long bytes_deallocated;
+  unsigned long objects_allocated;
+  unsigned long objects_deallocated;
+};
+
 //thread context specific data
 typedef struct mutator_context MContext;
 struct mutator_context {
   GOptions *gopts; //pointer to global options. same for all threads
   MOptions opt; //thread local options
+  MStat *stat;
   unsigned int time;
 };
 
--- src/memory.c
+++ src/memory.c
@@ -1,6 +1,7 @@
+#include "acdc.h"
 #include "memory.h"
 
-void *allocate(size_t size) {
+void *allocate(size_t size, MContext *mc) {
 	void *ptr;
 	//TODO: log allocator activity
 	
@@ -13,14 +14,38 @@ void *allocate(size_t size) {
 
 	ptr = malloc(size);
 	
+	//set header information
 	Object *o = (Object*)ptr;
+	o->rctm = 0;
 	o->size = size;
+
+	//update mutator stats
+	mc->stat->bytes_allocated += size;
+	mc->stat->objects_allocated++;
 
 	return ptr;
 }
 
-void deallocate(void *ptr) {
-	//TODO: log allocator activity
+void deallocate(void *ptr, MContext *mc) {
+	Object *o = (Object*)ptr;
+
+	//update mutator stats
+	mc->stat->bytes_deallocated += o->size;
+	mc->stat->objects_deallocated++;
+
 	free(ptr);
 }
+
+
+void access_object(Object *o) {
+	int i;
+	//payload size can be calculated from object and header size
+	size_t pl_sz = o->size - sizeof(Object) + sizeof(char*);
+	
+	for (i = 1; i < pl_sz; ++i) {
+		o->payload[i] = (o->payload[i-1]) + 1;
+	}
+}
+
+
 
