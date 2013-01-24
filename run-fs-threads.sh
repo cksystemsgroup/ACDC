ac851e6ecaa34099089882af58868b166ae6f117
Martin Aigner <martin@maigner.net>
2013-01-24 13:23:03 +0100

    added llalloc

--- run-fs-threads.sh
+++ run-fs-threads.sh
@@ -5,21 +5,21 @@ OPTIONS="-f -s 2 -S 4 -d 50 -i 1000000"
 REPS=10
 echo "#ACDC Options: $OPTIONS"
 echo "#Created at: `date` on `hostname`"
-echo -e "#threads\tjemalloc\tstddev\toptimal\tstddev\tptmalloc2\tstddev\tptmalloc3\tstddev\ttbb\tstddev\ttcmalloc\tstddev"
+echo -e "#threads\tjemalloc\tstddev\tllalloc\tstddev\toptimal\tstddev\tptmalloc2\tstddev\tptmalloc3\tstddev\ttbb\tstddev\ttcmalloc\tstddev"
 for THREADS in 1 2 3 4 5 6 7 8
 do
 	OUTPUT="$THREADS"
-	for CONF in jemalloc optimal ptmalloc2 ptmalloc3 tbb tcmalloc
+	for CONF in jemalloc llalloc optimal ptmalloc2 ptmalloc3 tbb tcmalloc
 	do
 		VALUE_SUM=0
 		for (( REP=1; REP<=$REPS; REP++ ))
 		do
 			RESULT=$(./build/acdc-$CONF $OPTIONS -r $REP -n $THREADS | grep RUNTIME)
 			read -a ARRAY <<<$RESULT
-			VALUE[$REP]=$(echo "scale=1;${ARRAY[9]}/$THREADS" | bc)
-			VALUE_SUM=$(echo "${VALUE_SUM}+${VALUE[$REP]}" | bc)
+			VALUE[$REP]=$(echo "scale=1; ${ARRAY[9]} / $THREADS" | bc)
+			VALUE_SUM=$(echo "$VALUE_SUM + ${VALUE[$REP]}" | bc)
 		done
-		AVG=$(echo "scale=1;${VALUE_SUM}/5" | bc)
+		AVG=$(echo "scale=1;$VALUE_SUM / $REPS" | bc)
 		SSD=0
 		for (( REP=1; REP<=$REPS; REP++ ))
 		do
