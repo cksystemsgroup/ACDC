ff5fa03ada4cbb448eb0ff39063244b32cb96a52
Martin Aigner <martin@maigner.net>
2013-01-24 13:58:09 +0100

    added interface for resource usage (memory)

---
+++ src/resources.h
@@ -0,0 +1,16 @@
+ /*
+ * Copyright (c) 2012, the ACDC Project Authors.
+ * All rights reserved. Please see the AUTHORS file for details.
+ * Use of this source code is governed by a BSD license that
+ * can be found in the LICENSE file.
+ */
+
+
+#ifndef RESOURCES_H
+#define RESOURCES_H
+
+long get_resident_set_size();
+long get_high_water_mark();
+long get_data_segment_size();
+
+#endif
