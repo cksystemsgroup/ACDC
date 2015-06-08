c2027f513c1f37ac55930fa1795fd4349663f467
Michael Lippautz <michael.lippautz@gmail.com>
2015-06-08 20:44:46 +0200

    Checkout release tag for scalloc

--- install_allocators.sh
+++ install_allocators.sh
@@ -114,6 +114,7 @@ function install_allocator {
                     rm -rf libscalloc.so
                     git clone https://github.com/cksystemsgroup/scalloc.git
                     cd scalloc
+                    git checkout release
                     tools/make_deps.sh
                     build/gyp/gyp --depth=. scalloc.gyp
                     BUILDTYPE=Release make
